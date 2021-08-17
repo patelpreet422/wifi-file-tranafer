f7c02e660cc0486acff3aa44253cec3800ed85fa
Preet Patel <patelpreet422@gmail.com>
2021-08-18 01:10:25 +0530

    add util to get list of all IP addresses

--- main.go
+++ main.go
@@ -3,17 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/patelpreet422/wifi-file-transfer/cmd"
+	"github.com/patelpreet422/wifi-file-transfer/util"
 )
 
 func main() {
-	args, err := cmd.ParseCommandLineArgs()
-
-	fmt.Printf("command: %v, port: %d, files: %v\n", args.Command, args.Port, args.Files)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		fmt.Printf("See you later, bye 👋.\n")
-		return
-	}
+	ip, _ := util.GetIPAddr()
+	fmt.Println("ip: %v\n", ip)
 }
---
+++ util/util.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"errors"
+	"net"
+)
+
+func GetIPAddr() (string, error) {
+	ifaces, err := net.Interfaces()
+
+	if err != nil {
+		return "", errors.New("util.getIPAddr(): Failed to get net interfaces")
+	}
+
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", errors.New("util.getIPAddr(): Failed to get IP addresses of interface")
+		}
+
+		// Each interface can have both IPv4 and IPv6
+		for _, addr := range addrs {
+			var ip net.IP
+
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if !ip.IsLoopback() {
+				return ip.String(), nil
+			}
+		}
+	}
+	return "", errors.New("util.getIPAddr(): No local IP found")
+
+}
